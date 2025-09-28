8a15abf0f79684655e0eb47cafe061056033a158
Abror <alisherabror2023@gmail.com>
2025-09-28 15:13:39 +0500

    first commit

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.json({
-        message:"BEKEND ISHLADI"
+        message: "BEKEND ISHLADI"
     })
 })
 
